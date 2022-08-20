be91e0f1023efe878f542bff77580a3392502626
alexandermehks <alexandermehks@gmail.com>
2022-08-20 22:11:35 +0200

    update

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::net::TcpListener;
 pub struct Server {
         addr: String,
     }
@@ -8,6 +9,17 @@ pub struct Server {
 
         pub fn run(self){
             println!("Server is listening on {}.", self.addr);
+            let listener = TcpListener::bind(&self.addr).unwrap();
+
+            loop{
+                match listener.accept(){
+                    Ok((stream, addr)) => {
+                        println!("Connection successful from: {}", addr);
+                    },
+                    Err(e) => println!("Failed to establish a connection: {}", e),
+                }
+            }
+
         }
 
     }
